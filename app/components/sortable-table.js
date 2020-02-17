b461e1ffbd302df2aff6c83fa8d94ecde059d159
Satish Kumar <satiskum@ciena.com>
2020-02-17 20:08:17 +0530

    table changes

--- app/components/sortable-table.js
+++ app/components/sortable-table.js
@@ -15,12 +15,72 @@ export default Component.extend({
   getCellValue(row, index){ return $(row).children('td').eq(index).text() },
 
   actions: {
-    sortTable() {
+    sortTable () {
       var table = $('#checkTable').eq(0)
       var rows = table.find('tr:gt(0)').toArray().sort(this.comparer(this, table.index()))
       this.asc = !this.asc
       if (!this.asc){rows = rows.reverse()}
       for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+    },
+
+    sortUpTime (n) {
+      var table;
+      table = document.getElementById("checkTable");
+      var rows, i, x, y, count = 0;
+      var switching = true;
+
+      // Order is set as ascending
+      var direction = "ascending";
+
+      // Run loop until no switching is needed
+      while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        //Loop to go through all rows
+        for (i = 1; i < (rows.length - 1); i++) {
+          var Switch = false;
+
+          // Fetch 2 elements that need to be compared
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+
+          // Check the direction of order
+          if (direction == "ascending") {
+
+            // Check if 2 rows need to be switched
+            if (parseFloat(x.innerHTML.toLowerCase()) > parseFloat(y.innerHTML.toLowerCase()))
+            {
+              // If yes, mark Switch as needed and break loop
+              Switch = true;
+              break;
+            }
+          } else if (direction == "descending") {
+
+            // Check direction
+            if (parseFloat(x.innerHTML.toLowerCase()) < parseFloat(y.innerHTML.toLowerCase()))
+            {
+              // If yes, mark Switch as needed and break loop
+              Switch = true;
+              break;
+            }
+          }
+        }
+        if (Switch) {
+          // Function to switch rows and mark switch as completed
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+
+          // Increase count for each switch
+          count++;
+        } else {
+          // Run while loop again for descending order
+          if (count == 0 && direction == "ascending") {
+            direction = "descending";
+            switching = true;
+          }
+        }
+      }
     }
   }
 });

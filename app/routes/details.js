be6800d9b6ffc4027ded6c4438c180ee57ae9644
Satish Kumar <satiskum@ciena.com>
2020-02-18 16:50:39 +0530

    code cleanup

--- app/routes/details.js
+++ app/routes/details.js
@@ -1,11 +1,8 @@
 import Route from '@ember/routing/route';
 import {isNone} from '@ember/utils'
-import {inject as service} from '@ember/service'
 import {DESCRIPTIONS} from 'status-app/constants/details'
 
 export default Route.extend({
-  // == Dependencies ==========================================================
-  ajax: service('ajax'),
 
   // == Ember Lifecycle Hooks =================================================
   setupController(controller) {
@@ -14,13 +11,9 @@ export default Route.extend({
 
     if (!isNone(controller.model)) {
       token = controller.model.token
-      const check = this.store.peekRecord('check', token)
 
-      controller.setProperties({
-        alias: check.alias,
-        uptime: check.uptime,
-        check: check
-      })
+      const check = this.store.peekRecord('check', token)
+      controller.set('check', check)
 
       const description = DESCRIPTIONS[token]
 
